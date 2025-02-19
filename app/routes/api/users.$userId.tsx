2fd94dc43848a857447bda1a8cba213e32e91acb
talmosko <tal.moskovich@gmail.com>
2025-02-19 16:18:55 +0200

    Remove debug console log in user API route

--- app/routes/api/users.$userId.tsx
+++ app/routes/api/users.$userId.tsx
@@ -4,7 +4,6 @@ import { createAPIFileRoute } from "@tanstack/start/api";
 
 export const APIRoute = createAPIFileRoute("/api/users/$userId")({
   GET: async ({ request, params }) => {
-    console.log("fetching user", params.userId);
     const user = await getUserById(Number(params.userId));
     return json({ ...user, date: new Date() });
   },
